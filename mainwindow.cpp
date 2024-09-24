ff46a3a0b313f07dd9d5442521e16cf25da104c0
lyh552506 <1661650252@qq.com>
2024-09-24 16:54:06 +0800

    [File] add open/save trigger

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,105 +1,125 @@
 #include "mainwindow.h"
+#include <qevent.h>
 
 GraphicsView::GraphicsView(QGraphicsScene *scene, QWidget *parent = nullptr)
-    : QGraphicsView(scene, parent), currentComponent(nullptr), currentWire(nullptr), isPlacingComponent(false), isPlacingWire(false) {}
+    : QGraphicsView(scene, parent),
+      currentComponent(nullptr),
+      currentWire(nullptr),
+      isPlacingComponent(false),
+      isPlacingWire(false) {}
 
 void GraphicsView::mousePressEvent(QMouseEvent *event) {
-    if (event->button() == Qt::LeftButton) {
-        if (isPlacingComponent) {
-            // 放置元件
-            Component *newComponent = new Resistor();
-            scene()->addItem(newComponent);
-            newComponent->setPos(mapToScene(event->pos()));
-            isPlacingComponent = false;
-        } else if (isPlacingWire) {
-            // 开始放置连接线
-            if (currentWire == nullptr) {
-                currentWire = new Wire();
-                scene()->addItem(currentWire);
-                currentWire->setPos(mapToScene(event->pos()));
-            }
-        }
+  if (event->button() == Qt::LeftButton) {
+    if (isPlacingComponent) {
+      // 放置元件
+      Component *newComponent = new Resistor();
+      scene()->addItem(newComponent);
+      newComponent->setPos(mapToScene(event->pos()));
+      isPlacingComponent = false;
+    } else if (isPlacingWire) {
+      // 开始放置连接线
+      if (currentWire == nullptr) {
+        currentWire = new Wire();
+        scene()->addItem(currentWire);
+        currentWire->setPos(mapToScene(event->pos()));
+      }
     }
+  }
 }
 void GraphicsView::mouseMoveEvent(QMouseEvent *event) {
-    if (isPlacingWire && currentWire != nullptr) {
-        currentWire->updateGeometry();
-    }
+  if (isPlacingWire && currentWire != nullptr) {
+    currentWire->updateGeometry();
+  }
 }
 void GraphicsView::mouseReleaseEvent(QMouseEvent *event) {
-    if (isPlacingWire && currentWire != nullptr) {
-        currentWire->updateGeometry();
-        isPlacingWire = false;
-    }
+  if (isPlacingWire && currentWire != nullptr) {
+    currentWire->updateGeometry();
+    isPlacingWire = false;
+  }
 }
-void GraphicsView::placeComponent() {
-    isPlacingComponent = true;
-}
-void GraphicsView::placeWire() {
-    isPlacingWire = true;
-}
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent) {
-    InitToolBar();
-    InitGraph();
+void GraphicsView::placeComponent() { isPlacingComponent = true; }
+void GraphicsView::placeWire() { isPlacingWire = true; }
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+  InitToolBar();
+  InitGraph();
 }
 
-MainWindow::~MainWindow() {
-}
+MainWindow::~MainWindow() {}
 
 void MainWindow::InitToolBar() {
-    menuToolBar = menuBar()->addMenu(tr("&ToolBar"));
-    menuTest_put = menuBar()->addMenu(tr("&Put"));
-    menuTest_file = menuBar()->addMenu(tr("&File"));
-    
-    toolBar_put = new QToolBar(tr("&put"), this);
-    toolBar_file = new QToolBar(tr("&file"),this);
+  menuToolBar = menuBar()->addMenu(tr("&ToolBar"));
+  menuTest_put = menuBar()->addMenu(tr("&Put"));
+  menuTest_file = menuBar()->addMenu(tr("&File"));
 
-    actionTest_put = new QAction(tr("&Put tool"), this);
-    actionTest_file = new QAction(tr("&File tool"), this);
-    
-    menuToolBar->addAction(actionTest_put);
-    menuToolBar->addAction(actionTest_file);
+  toolBar_put = new QToolBar(tr("&put"), this);
+  toolBar_file = new QToolBar(tr("&file"), this);
 
-    put = new QAction(tr("Put"), this);
-    file = new QAction(tr("File"), this);
-    toolBar_put->addAction(put);
-    toolBar_file->addAction(file);
+  actionTest_put = new QAction(tr("&Put tool"), this);
+  actionTest_file = new QAction(tr("&File tool"), this);
+  action_Open = new QAction(tr("&Open"), this);
+  action_Save = new QAction(tr("&Save"), this);
+  // add ToolBar act
+  menuToolBar->addAction(actionTest_put);
+  menuToolBar->addAction(actionTest_file);
+  // add File act
+  menuTest_file->addAction(action_Open);
+  menuTest_file->addAction(action_Save);
 
-    connect(actionTest_put, &QAction::triggered, this, [=](){ this->showToolBar(toolBar_put); });
-    connect(actionTest_file, &QAction::triggered, this, [=](){ this->showToolBar(toolBar_file); });
+  put = new QAction(tr("Put"), this);
+  file = new QAction(tr("File"), this);
+  toolBar_put->addAction(put);
+  toolBar_file->addAction(file);
+  // bind Put and File bar
+  connect(actionTest_put, &QAction::triggered, this,
+          [=]() { this->showToolBar(toolBar_put); });
+  connect(actionTest_file, &QAction::triggered, this,
+          [=]() { this->showToolBar(toolBar_file); });
+  // bind open and save bar
+  connect(action_Open, &QAction::triggered, this,
+          &MainWindow::open_file_triggered);
+  connect(action_Save, &QAction::triggered, this,
+          &MainWindow::save_file_triggered);
 
-    connect(put, &QAction::triggered, this, &MainWindow::on_put_triggered);
-    connect(file, &QAction::triggered, this, &MainWindow::on_file_triggered);
+  connect(put, &QAction::triggered, this, &MainWindow::on_put_triggered);
+  connect(file, &QAction::triggered, this, &MainWindow::on_file_triggered);
 
-    this->addToolBar(Qt::LeftToolBarArea, toolBar_put);
-    this->addToolBar(Qt::LeftToolBarArea, toolBar_file);
+  this->addToolBar(Qt::LeftToolBarArea, toolBar_put);
+  this->addToolBar(Qt::LeftToolBarArea, toolBar_file);
 
-    resize(1000, 800);
+  resize(1000, 800);
 }
-void MainWindow::showToolBar(QToolBar* toolBar) {
-    foreach(QToolBar* bar, findChildren<QToolBar*>()) {
-        bar->setVisible(bar == toolBar);
-    }
+void MainWindow::showToolBar(QToolBar *toolBar) {
+  foreach (QToolBar *bar, findChildren<QToolBar *>()) {
+    bar->setVisible(bar == toolBar);
+  }
 }
 
 void MainWindow::InitGraph() {
-    scene = new QGraphicsScene(this);
-    scene->setSceneRect(-100,-100,400,400);
-    view = new GraphicsView(scene, this);
-    view->setRenderHint(QPainter::Antialiasing);
-    view->setFixedSize(800,600);
-    
-    setCentralWidget(view);
+  scene = new QGraphicsScene(this);
+  scene->setSceneRect(-100, -100, 400, 400);
+  view = new GraphicsView(scene, this);
+  view->setRenderHint(QPainter::Antialiasing);
+  view->setFixedSize(800, 600);
+
+  setCentralWidget(view);
 }
 void MainWindow::on_put_triggered() {
-    qDebug() << "Test put";
-    RectangleItem* item = new RectangleItem();
-    scene->addItem(item);
-    item->setPos(QPointF(100,100));
-    
+  qDebug() << "Test put";
+  RectangleItem *item = new RectangleItem();
+  scene->addItem(item);
+  item->setPos(QPointF(100, 100));
 }
 
-void MainWindow::on_file_triggered() {
-    qDebug() << "Test file";
+void MainWindow::open_file_triggered() {
+  qDebug() << "Open Test";
+  auto file_name = QFileDialog::getOpenFileName(
+      this, tr("Open File"), "", tr("All Files (*);;Text Files (*.txt)"));
+  if(!file_name.isEmpty()){
+    QFile target(file_name);
+    //TODO实现打开文件后的操作
+  }    
 }
+
+void MainWindow::save_file_triggered() { qDebug() << "Save Test"; }
+
+void MainWindow::on_file_triggered() { qDebug() << "Test file"; }
--- mainwindow.h
+++ mainwindow.h
@@ -1,68 +1,81 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-#include <QToolBar>
+#include <qaction.h>
+#include <qchar.h>
 #include <QAction>
-#include <QMenuBar>
 #include <QDebug>
-#include <QMouseEvent>
-#include <QGraphicsView>
-#include <QGraphicsScene>
+#include <QFile>
+#include <QFileDialog>
 #include <QGraphicsItem>
+#include <QGraphicsScene>
 #include <QGraphicsSceneMouseEvent>
-#include "Rectangle.hpp"
+#include <QGraphicsView>
+#include <QMainWindow>
+#include <QMenuBar>
+#include <QMessageBox>
+#include <QMouseEvent>
+#include <QTextStream>
+#include <QToolBar>
 #include "Component.hpp"
-#include "Wire.hpp"
+#include "Rectangle.hpp"
 #include "Resistor.hpp"
+#include "Wire.hpp"
+
 class GraphicsView : public QGraphicsView {
-public:
-    GraphicsView(QGraphicsScene*, QWidget* parent);
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
-    void placeComponent();
-    void placeWire() ;
-private:
-    Component *currentComponent;
-    Wire *currentWire;
-    bool isPlacingComponent;
-    bool isPlacingWire;
+ public:
+  GraphicsView(QGraphicsScene*, QWidget* parent);
+  void mousePressEvent(QMouseEvent* event) override;
+  void mouseMoveEvent(QMouseEvent* event) override;
+  void mouseReleaseEvent(QMouseEvent* event) override;
+  void placeComponent();
+  void placeWire();
+
+ private:
+  Component* currentComponent;
+  Wire* currentWire;
+  bool isPlacingComponent;
+  bool isPlacingWire;
 };
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+ public:
+  MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
 
-    void InitToolBar();
-    void showToolBar(QToolBar*);
+  void InitToolBar();
+  void showToolBar(QToolBar*);
 
-    void InitGraph();
+  void InitGraph();
 
-private:
-    // QMenuBar* menuBar;
-    QMenu* menuToolBar;
-    QMenu* menuTest_put;
-    QMenu* menuTest_file;
+ private:
+  // QMenuBar* menuBar;
+  QMenu* menuToolBar;
+  QMenu* menuTest_put;
+  QMenu* menuTest_file;
 
-    QToolBar* toolBar;
-    QToolBar* toolBar_put;
-    QToolBar* toolBar_file;
+  QToolBar* toolBar;
+  QToolBar* toolBar_put;
+  QToolBar* toolBar_file;
 
-    QAction* actionTest;
-    QAction* actionTest_put;
-    QAction* actionTest_file;
-    QAction* put;
-    QAction* file;
+  QAction* actionTest;
+  QAction* actionTest_put;
+  QAction* actionTest_file;
+  QAction* action_Open;
+  QAction* action_Save;
+  QAction* put;
+  QAction* file;
 
-    QGraphicsScene* scene;
-    QGraphicsView* view;
-private slots:
-    void on_put_triggered();
-    void on_file_triggered();
+  QGraphicsScene* scene;
+  QGraphicsView* view;
+
+  QString current_file;
+ private slots:
+  void on_put_triggered();
+  void on_file_triggered();
+  void open_file_triggered();
+  void save_file_triggered();
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
