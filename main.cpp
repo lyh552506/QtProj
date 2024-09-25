2daa3d160eba3567a81a1e1ae50fc52be1624dbf
RicoAfoat <51285519+RicoAfoat@users.noreply.github.com>
2024-09-25 21:40:51 +0800

    [Light Mode]

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <qstylefactory.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    
+    // 设置浅色模式
+    QPalette lightPalette;
+    lightPalette.setColor(QPalette::Window, Qt::white);
+    lightPalette.setColor(QPalette::WindowText, Qt::black);
+    lightPalette.setColor(QPalette::Base, Qt::white);
+    lightPalette.setColor(QPalette::AlternateBase, Qt::lightGray);
+    lightPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    lightPalette.setColor(QPalette::ToolTipText, Qt::black);
+    lightPalette.setColor(QPalette::Text, Qt::black);
+    lightPalette.setColor(QPalette::Button, Qt::white);
+    lightPalette.setColor(QPalette::ButtonText, Qt::black);
+    lightPalette.setColor(QPalette::BrightText, Qt::red);
+    lightPalette.setColor(QPalette::Link, Qt::blue);
+    lightPalette.setColor(QPalette::Highlight, Qt::blue);
+    lightPalette.setColor(QPalette::HighlightedText, Qt::white);
+    a.setPalette(lightPalette);
+    a.setStyle(QStyleFactory::create("Fusion"));
+
     MainWindow w;
     w.show();
     return a.exec();
